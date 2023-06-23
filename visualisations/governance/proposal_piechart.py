a6a83f2447e2b02ffb129a73d3dc974beff63b80
Orogun Okunola Adebola <98488336+Adebolaorogun@users.noreply.github.com>
2023-06-23 19:53:37 +0100

    Update proposal_piechart.py

--- visualisations/governance/proposal_piechart.py
+++ visualisations/governance/proposal_piechart.py
@@ -4,20 +4,16 @@ import plotly.graph_objects as go
 
 data = pd.read_csv('simulated_data.csv')
 def proposal_interaction_range_pie_chart(total_proposal_range):
-    # Filter the data based on the specified total proposal interaction range
+ 
     filtered_data = data[(data['totalProposalInteraction'] >= total_proposal_range[0]) &
                          (data['totalProposalInteraction'] <= total_proposal_range[1])]
 
-    # Get the filtered ENS names
     filtered_ens_names = filtered_data['ensName'].unique()
 
-    # Calculate the percentage of ENS names in the filtered data compared to the total ENS names
     filtered_percentage = len(filtered_ens_names) / len(data['ensName'].unique()) * 100
 
-    # Calculate the percentage of ENS names not in the filtered data
     remaining_percentage = 100 - filtered_percentage
 
-    # Create a pie chart to visualize the percentages
     labels = ['filtered Ens names', 'Remaining ENS Names']
     values = [filtered_percentage, remaining_percentage]
 
